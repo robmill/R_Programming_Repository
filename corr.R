eb985152a341991c14257e89d6f1ab7cfb2abd82
robmill <r_miller213@hotmail.com>
2016-07-14 16:21:07 -0700

    Corr Edit

--- corr.R
+++ corr.R
@@ -1,4 +1,5 @@
 # Crrelation of complete cases
+source("c:/users/robmill/Documents/Github/R_Programming_Repository/completeCases.R")
 
 corr<-function(directory, threshold=0){
         
@@ -10,28 +11,14 @@ corr<-function(directory, threshold=0){
         # subset dataframe df by id monitors
         df<-as.data.frame(list.files(path=".", pattern="^[0-3]"), 
                           stringsAsFactors=FALSE)
-        df<-as.data.frame(df[id,],stringsAsFactors=FALSE)
         
-        df_complete<-complete(directory)
+        df_complete<-as.data.frame(complete(directory))
         df_complete<-df_complete[df_complete$nobs>threshold,]
 
         
         
-        # capture list of monitor files in dataframe df
-        # subset dataframe df by id monitors
-        df<-as.data.frame(list.files(path=".", pattern="^[0-3]"), 
-                          stringsAsFactors=FALSE)
-        df<-as.data.frame(df[id,],stringsAsFactors=FALSE)
-        
-        # instantiate monitor
-        # run for loop to read in monitor file
-        # and rbind to monitor
-        monitor<-NULL
-        for (i in 1:nrow(df)) {
-                temp<-read.csv(df[i,])
-                monitor<-rbind(monitor,temp)
-        }
+
 
         
-        
+        df_complete
 }
